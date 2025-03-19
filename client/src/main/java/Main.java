30e6e4de28419e9bd5158206337dd4130e5d223f
Joshua Neely <joshua15neely@gmail.com>
2025-03-18 20:43:39 -0600

    Beginning project

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,6 +2,13 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
+        var serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+        new Repl(serverUrl).run();
+
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
     }
---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,11 @@
+package client;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    private final PreClient preclient;
+    // private final PostClient postclient;
+
+}
