e20768933cde7f0ad3f17bd3cef61d909576874b
Joshua Neely <joshua15neely@gmail.com>
2025-03-24 23:47:16 -0600

    can join

--- client/src/main/java/client/MainClient.java
+++ client/src/main/java/client/MainClient.java
@@ -1,5 +1,7 @@
 package client;
 
+import model.AuthData;
+import model.GameData;
 import server.ServerFacade;
 
 import java.util.Arrays;
@@ -27,7 +29,7 @@ public class MainClient implements Client {
                 Options:
                 List all games: "listGames"
                 Create a game: "createGame" <GAME_NAME>
-                Join a game: "joinGame" <GAME_ID>
+                Join a game: "joinGame" <PLAYER_COLOR> <GAME_ID>
                 Logout: "logout"
                 Print this message: "help"
                 """;
@@ -36,10 +38,12 @@ public class MainClient implements Client {
     @Override
     public String eval(String input) {
         try {
-            var tokens = input.toLowerCase().split(" ");
+            var tokens = input.split(" ");
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
+                case "listGames" -> listGames();
+                case "createGame" -> createGame(params);
                 case "joinGame" -> joinGame(params);
                 case "logout" -> "logout";
                 default -> help();
@@ -59,7 +63,46 @@ public class MainClient implements Client {
         this.state = state;
     }
 
-    private String joinGame(String... params) {
-        return "";
+    private String listGames() throws Exception {
+        try {
+            GameData[] games = server.listGames(token);
+            System.out.println("\nGames:");
+            for (GameData game: games) {
+                System.out.println(
+                        "ID: " + game.getGameID()
+                                + " NAME: " + game.getGameName()
+                                + " WHITE: " + game.getWhiteUsername()
+                                + " BLACK: " + game.getBlackUsername() + "\n");
+            }
+            return String.format("Successfully listed Games");
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+    private String createGame(String... params) throws Exception {
+        if (params.length == 1) {
+            try {
+                server.createGame(token, params[0]);
+                return String.format("Created game %s", params[0]);
+            } catch (Exception ex) {
+                throw new Exception(ex.getMessage());
+            }
+        }
+        throw new Exception("Expected: <GAME_NAME>");
+    }
+
+    private String joinGame(String... params) throws Exception {
+        if (params.length == 2) {
+            try {
+                params[0] = params[0].toUpperCase();
+                server.joinGame(this.token, params[0], Integer.parseInt(params[1]));
+                state = State.IN_GAME;
+                return String.format("Successfully joined game: %s.", params[0]);
+            } catch (Exception ex) {
+                throw new Exception(ex.getMessage());
+            }
+        }
+        throw new Exception("Expected: <PLAYER_COLOR> <GAME_ID>");
     }
 }
