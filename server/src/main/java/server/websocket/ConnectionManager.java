d760d72f254e7c961bd270dc4e93f51620a76600
Joshua Neely <joshua15neely@gmail.com>
2025-04-01 16:18:49 -0600

    wowow :

---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,44 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    // Adds a connection to the WebSocket
+    public void add(String authToken, Session session) {
+        var connection = new Connection(authToken, session);
+        connections.put(authToken, connection);
+    }
+
+    // Removes a connection from the WebSocket
+    public void remove(String authToken) {
+        connections.remove(authToken);
+    }
+
+    // Broadcasts to all connections to the WebSocket
+    public void broadcast(String excludeAuthToken, Notification notification) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+
+                // Excludes the user's token which made the broadcast
+                if (!c.authToken.equals(excludeAuthToken)) {
+                    c.send(notification.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            connections.remove(c.authToken);
+        }
+    }
+}
